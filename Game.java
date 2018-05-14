5bb1aac389325814c73755890277cd4656ec043b
FALOSW <171wita24@o365.htl-leoben.at>
2018-05-14 14:35:32 +0200

    Spielfeld

--- Game.java
+++ Game.java
@@ -1,12 +1,15 @@
 
 public class Game {
 
+	static int _zeilen = 3;
+	static int _spalten = 3;
+
 	public static void main(String[] args) {
-		String[][] feld = spielfeldAnlegen(3,3);
+		String[][] feld = spielfeldAnlegen(3, 3);
 		// feld = mineSetzen(0,3);
-		System.out.println(feld.toString());
+		SpielfeldAnzeigen(feld, _zeilen, _spalten);
 	}
-	
+
 	public static String[][] spielfeldAnlegen(int zeilen, int spalten) {
 		String[][] mineField = new String[zeilen][spalten];
 		for (int z = 0; z < zeilen; z++) {
@@ -16,6 +19,14 @@ public class Game {
 		}
 		return mineField;
 	}
-	
-	
+
+	public static void SpielfeldAnzeigen(String[][] Feld, int zeilen, int spalten) {
+		for (int z = 0; z < zeilen; z++) {
+			for (int s = 0; s < spalten; s++) {
+				System.out.print(Feld[z][s]);
+			}
+			System.out.println();
+		}
+
+	}
 }
